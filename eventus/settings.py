94e1b613e70f6864e133c3e57c64999fbfd7ed17
guvdutra <guvadutra@gmail.com>
2024-12-10 08:43:31 -0300

    Alterado pagina admin para portugues de forma correta

--- eventus/settings.py
+++ eventus/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
@@ -128,5 +128,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LANGUAGE_CODE = 'pt-BR'
