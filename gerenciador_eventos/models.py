4f41ce5e821d6e2e80caa0150aedd853555339f0
Gustavo Viana Avelar Dutra <106356253+guvdutra@users.noreply.github.com>
2024-12-11 15:39:02 -0300

    Update models.py

--- gerenciador_eventos/models.py
+++ gerenciador_eventos/models.py
@@ -1,22 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Modelo de Evento
 class Evento(models.Model):
-  titulo = models.CharField(max_length=100)  # Nome do evento
-  descricao = models.TextField()  # Descrição do evento
-  local = models.CharField(max_length=255)  # Localização do evento
-  data = models.DateTimeField()  # Data e hora do evento
-  organizador = models.ForeignKey(User, related_name='eventos_criados', on_delete=models.CASCADE)  # O organizador do evento (relacionamento com User)
+  titulo = models.CharField(max_length=100)
+  descricao = models.TextField()
+  local = models.CharField(max_length=255)
+  data = models.DateTimeField()
+  organizador = models.ForeignKey(User, related_name='eventos_criados', on_delete=models.CASCADE)
 
   def __str__(self):
     return self.titulo
 
-# Modelo de Participação (relaciona usuários com eventos e define o organizador)
 class Participacao(models.Model):
-  usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Relacionamento com o usuário
-  evento = models.ForeignKey(Evento, related_name='participantes', on_delete=models.CASCADE)  # Relacionamento com o evento
-  eh_organizador = models.BooleanField(default=False)  # Define se o usuário é o organizador do evento
+  usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+  evento = models.ForeignKey(Evento, related_name='participantes', on_delete=models.CASCADE)
+  eh_organizador = models.BooleanField(default=False)
 
   def __str__(self):
     return f'{self.usuario.username} - {self.evento.titulo}'
