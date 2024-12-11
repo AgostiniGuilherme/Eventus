94ff0762ee3e1d6cd41d69e4aec7fdb78ec02ab9
Gustavo Viana Avelar Dutra <106356253+guvdutra@users.noreply.github.com>
2024-12-11 15:41:37 -0300

    Update admin.py

--- gerenciador_eventos/admin.py
+++ gerenciador_eventos/admin.py
@@ -1,20 +1,16 @@
 from django.contrib import admin
 from .models import Evento, Participacao
 
-# Registra o modelo Evento
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'local', 'data', 'organizador')  # O que vai ser exibido na lista de eventos
-    search_fields = ('titulo', 'descricao', 'local')  # Campos em que será possível realizar buscas
-    list_filter = ('data', 'local')  # Filtros que podem ser aplicados
-    date_hierarchy = 'data'  # Permite filtrar por data de forma hierárquica
+    list_display = ('titulo', 'local', 'data', 'organizador')
+    search_fields = ('titulo', 'descricao', 'local')
+    list_filter = ('data', 'local')
+    date_hierarchy = 'data'
 
-
-# Registra o modelo Participacao
 class ParticipacaoAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'evento', 'eh_organizador')  # O que será exibido na lista de participações
-    search_fields = ('usuario__username', 'evento__titulo')  # Possibilita buscas pelo nome do usuário e evento
-    raw_id_fields = ('usuario', 'evento')  # Usar ID direto para campos de relacionamento
+    list_display = ('usuario', 'evento', 'eh_organizador')
+    search_fields = ('usuario__username', 'evento__titulo')
+    raw_id_fields = ('usuario', 'evento')
 
-# Registra os modelos no painel administrativo
 admin.site.register(Evento, EventoAdmin)
 admin.site.register(Participacao, ParticipacaoAdmin)
